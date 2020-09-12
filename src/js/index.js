1d3a4072d1072784eb900ef31ce8f0c3f1317021
Aramex86 <slavbez@mail.ru>
2020-09-12 19:47:45 +0300

    besctop version

---
+++ src/js/index.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('.slider-container').bxSlider({
+        pager:false,
+        moveSlides:1,
+        minSlides:1,
+        touchEnabled:false,
+        controls:true,
+        slideMargin:0
+    });
+    
+    $(".bx-wrapper").css({
+        "border":"none",
+        "box-shadow":'none',
+        'margin':0
+    });
+
+
+  });
