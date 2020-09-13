6f8834e38b747ae86c8c53b9a5637573e2370a4e
Aramex86 <slavbez@mail.ru>
2020-09-13 21:36:13 +0300

    media 320

--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,54 @@
-$(function(){
-    $('.slider-container').bxSlider({
-        pager:false,
-        moveSlides:1,
-        minSlides:1,
-        touchEnabled:false,
-        controls:true,
-        slideMargin:0
-    });
+$(function () {
+
+    //Slider
+  $(".slider-container").bxSlider({
+    pager: false,
+    moveSlides: 1,
+    minSlides: 1,
+    maxSlides: 1,
+    touchEnabled: true,
+    controls: true,
+    slideMargin: 0,
+    responsive: true,
+  });
+
+  $(".bx-wrapper").css({
+    border: "none",
+    "box-shadow": "none",
+    margin: 0,
+  });
+
+  $(document).on("click", ".burger-menu", function () {
+    $(".nav").slideToggle();
+    $(".nav").css("display", "flex");
+  });
+
+  //if()
+//   $(document).on("click", ".footer-heading:eq(0)", function () {
+//     $(".newsleter").slideToggle();
     
-    $(".bx-wrapper").css({
-        "border":"none",
-        "box-shadow":'none',
-        'margin':0
-    });
+//   });
+//   $(document).on("click", ".footer-heading:eq(1)", function () {
+//     $(".latestnews").slideToggle();
+    
+//   });
+//   $(document).on("click", ".footer-heading:eq(2)", function () {
+//     $(".tags").slideToggle();
+    
+//   });
+//   $(document).on("click", ".footer-heading:eq(3)", function () {
+//     $(".address").slideToggle();
+    
+//   });
+});
 
+const formBtn = document
+  .querySelector(".searchBtn")
+  .addEventListener("click", (e) => {
+    e.preventDefault();
   });
 
-
-
-
-  const formBtn = document.querySelector('.searchBtn').addEventListener('click',(e)=>{
-      e.preventDefault();
-  });
-  
-
-  const input = document.getElementById('datepicker');
-  const datepiker = new TheDatepicker.Datepicker(input);
-  datepiker.render();
+//datepiker
+const input = document.getElementById("datepicker");
+const datepiker = new TheDatepicker.Datepicker(input);
+datepiker.render();
